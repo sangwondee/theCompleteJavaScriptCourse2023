6017c9f7280353adeb70516cfdb4314cdcd0c4a9
AzureAD\WichanSangwondee <sangwondee@gmail.com>
2024-07-18 23:52:27 +0700

    game

---
+++ 05-Guess-My-Number/script.js
@@ -0,0 +1,43 @@
+'use strict';
+
+// console.log(document.querySelector('.message').textContent);
+// document.querySelector('.message').textContent = '😁 Correct Number';
+
+// document.querySelector('.number').textContent = 13
+// document.querySelector('.score').textContent = 1
+
+// document.querySelector('.guess').value = 23
+// console.log(document.querySelector('.guess').value)
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = secretNumber;
+
+let score = 20;
+
+
+document.querySelector('.check').addEventListener('click', function() {
+
+   const guess = Number(document.querySelector('.guess').value);
+
+   if (!guess) {
+    document.querySelector('.message').textContent = 'No number !'
+   } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '😁 Correct Number'
+   } else if (guess > secretNumber) {
+    if (score > 0) {
+        document.querySelector('.message').textContent = 'To hight'
+        score = score--;
+        document.querySelector('.score').textContent = score;
+    } else {
+        document.querySelector('.message').textContent = 'You lost the game!'
+    }
+
+    
+    
+
+   } else if (guess < secretNumber) {
+    document.querySelector('.message').textContent = 'To Low'
+    score = score--;
+    document.querySelector('.score').textContent = score;
+   }
+})
