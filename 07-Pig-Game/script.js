7e98b18f9166529e926b012a2e4d057ddd819194
wichas35 <wichas35@comex.postbox.in.th>
2024-07-23 00:17:45 +0700

    PROJECT #3: Pig Game

---
+++ 07-Pig-Game/script.js
@@ -0,0 +1,13 @@
+'use strict';
+
+// Selecting element
+
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+const dice = document.querySelector('.dice')
+
+// Starting conditions
+score0El.textContent = 0
+score1El.textContent = 0
+dice.classList.add('hidden');
+
