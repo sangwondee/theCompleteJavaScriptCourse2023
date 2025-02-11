e2f459c1ccbf1c11ee9a6a7109fda0f5b532f321
AzureAD\WichanSangwondee <sangwondee@gmail.com>
2025-02-11 08:53:38 +0700

    The This keyword in practice

--- 08-Behind-the-Scenes/script.js
+++ 08-Behind-the-Scenes/script.js
@@ -49,37 +49,79 @@
 // const year = 1991
 
 // Functions
-console.log(addDecl(2, 3));
+// console.log(addDecl(2, 3));
 // console.log(addExpr(2, 3));
-console.log(addArrow);
+// console.log(addArrow);
 
-// console.log(addArrow(2, 3));
+// // console.log(addArrow(2, 3));
 
-function addDecl(a, b) {
-  return a + b;
+// function addDecl(a, b) {
+//   return a + b;
+// }
+
+// const addExpr = function (a, b) {
+//   return a + b;
+// };
+
+// var addArrow = (a, b) => a + b;
+
+// // Example
+// console.log(undefined);
+// if (!numProducts) deleteShoppingCart();
+
+// var numProducts = 10;
+
+// function deleteShoppingCart() {
+//   console.log('All Products deleted !');
+// }
+
+// var x = 1;
+// let y = 2;
+// const z = 3;
+
+// console.log(x === window.x);
+// console.log(y === window.y);
+// console.log(z === window.z);
+
+
+
+// เรื่อง this keyword
+// console.log(this);
+
+//ถ้าใช้ใน function ธรรมดาในจะขึ้น undefined
+// const calcAge = function (birthYear) {
+//   console.log(2037 - birthYear);
+//   console.log(this);
+// }
+
+// calcAge(1991)
+
+// ถ้าใช้ใน arrow function มันจะใช้ได้
+// const calcAgeArrow = birthYear => {
+//   console.log(2037 - birthYear);
+//   console.log(this);
+// }
+
+// calcAgeArrow(1980)
+
+// เราสามารถเรียก function ใน Obj โดยใช้ this keyword นี้ได้
+const jonas = {
+  year : 1991,
+  calcAge : function () {
+    console.log(this);
+    console.log(2037 - this.year);
+  }
 }
 
-const addExpr = function (a, b) {
-  return a + b;
-};
+jonas.calcAge();
 
-var addArrow = (a, b) => a + b;
-
-// Example
-console.log(undefined);
-if (!numProducts) deleteShoppingCart();
-
-var numProducts = 10;
-
-function deleteShoppingCart() {
-  console.log('All Products deleted !');
+const matilda = {
+  year: 2017
 }
 
-var x = 1;
-let y = 2;
-const z = 3;
+// method borrowing
+matilda.calcAge = jonas.calcAge;
+matilda.calcAge()
 
-console.log(x === window.x);
-console.log(y === window.y);
-console.log(z === window.z);
-
+const f = jonas.calcAge
+f()
