2ec09de0df7e618fa3e415daedef439dc3b870f2
wichas35 <wichas35@comex.postbox.in.th>
2025-12-25 19:01:54 +0700

    Optional Chaining (?.)

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -1,24 +1,5 @@
 'use strict';
 
-///////////////////////////////////////
-// Coding Challenge #1
-
-/*
-We're building a football betting app (soccer for my American friends 😅)!
-
-Suppose we get data from a web service about a certain game (below). In this challenge we're gonna work with the data. So here are your tasks:
-
-1. Create one player array for each team (variables 'players1' and 'players2')
-2. The first player in any player array is the goalkeeper and the others are field players. For Bayern Munich (team 1) create one variable ('gk') with the goalkeeper's name, and one array ('fieldPlayers') with all the remaining 10 field players
-3. Create an array 'allPlayers' containing all players of both teams (22 players)
-4. During the game, Bayern Munich (team 1) used 3 substitute players. So create a new array ('players1Final') containing all the original team1 players plus 'Thiago', 'Coutinho' and 'Perisic'
-5. Based on the game.odds object, create one variable for each odd (called 'team1', 'draw' and 'team2')
-6. Write a function ('printGoals') that receives an arbitrary number of player names (NOT an array) and prints each of them to the console, along with the number of goals that were scored in total (number of player names passed in)
-7. The team with the lower odd is more likely to win. Print to the console which team is more likely to win, WITHOUT using an if/else statement or the ternary operator.
-
-TEST DATA FOR 6: Use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'. Then, call the function again with players from game.scored
-*/
-
 const game = {
   team1: 'Bayern Munich',
   team2: 'Borrussia Dortmund',
@@ -100,17 +81,16 @@ const teamLikelyToWin =
 
 const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
 
-
 const openingHours = {
   [weekdays[3]]: {
     open: 12,
     close: 22,
   },
-  [weekdays[4]]: {
+  [weekdays[3]]: {
     open: 11,
     close: 23,
   },
-  [weekdays[5]]: {
+  sat: {
     open: 0, // Open 24 hours
     close: 24,
   },
@@ -146,7 +126,36 @@ const restaurant = {
   },
 };
 
-console.log(restaurant);
+if (restaurant.openingHours && restaurant.openingHours.mon) console.log(restaurant.openingHours.mon.open);
+
+// WITH optional chaining
+// console.log(restaurant.openingHours.mon?.open);
+
+const days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+
+for (const day of days) {
+  const open = restaurant.openingHours[day]?.open ?? 'Closed'
+  console.log(`On ${day}, we open at ${open}`);
+}
+
+// Methods
+console.log(restaurant.order?.(0, 1) ?? 'Method does not exsit');
+console.log(restaurant.orderRisotto?.(0, 1) ?? 'Method does not exsit');
+
+// Arrays
+const users = [
+  {
+    name : 'wichan',
+    email : 'a@mail.com',
+  }
+]
+
+// const users = []
+console.log(users[0]?.name ?? 'User arry empty');
+// แบบเก่าจะเขียนแบบนี้
+
+if (users.length > 0) console.log(users[0]?.name);
+else console.log('User arry empty');
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Logical Assignment Operators
