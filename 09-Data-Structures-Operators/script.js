efac9d5178cd15bec583b59828cbae6472b80f64
wichas35 <wichas35@comex.postbox.in.th>
2026-01-01 22:52:54 +0700

    Looping Objects: Object Keys, Values, and Entries

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -86,11 +86,11 @@ const openingHours = {
     open: 12,
     close: 22,
   },
-  [weekdays[3]]: {
+  [weekdays[4]]: {
     open: 11,
     close: 23,
   },
-  sat: {
+  [weekdays[5]]: {
     open: 0, // Open 24 hours
     close: 24,
   },
@@ -126,21 +126,48 @@ const restaurant = {
   },
 };
 
+// Propety NAMES
+const properties = Object.keys(openingHours)
+console.log(properties);
+
+let openStr = `We are open on ${properties.length} days: ` ;
+
+for (const day of properties) {
+  openStr += `${day}, `;
+}
+
+console.log(openStr);
+
+// Propety VALUES
+const values = Object.values(openingHours)
+console.log(values);
+
+// Entire object
+const entries = Object.entries(openingHours);
+console.log(entries);
+
+// [key, value]
+for (const [day, { open, close }] of entries) {
+  console.log(`On ${day} we open at ${open} and close at ${close}`);
+}
+
+
+
 if (restaurant.openingHours && restaurant.openingHours.mon) console.log(restaurant.openingHours.mon.open);
 
 // WITH optional chaining
 // console.log(restaurant.openingHours.mon?.open);
 
-const days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+// const days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
 
-for (const day of days) {
-  const open = restaurant.openingHours[day]?.open ?? 'Closed'
-  console.log(`On ${day}, we open at ${open}`);
-}
+// for (const day of days) {
+//  const open = restaurant.openingHours[day]?.open ?? 'Closed'
+//  console.log(`On ${day}, we open at ${open}`);
+//}
 
 // Methods
-console.log(restaurant.order?.(0, 1) ?? 'Method does not exsit');
-console.log(restaurant.orderRisotto?.(0, 1) ?? 'Method does not exsit');
+//console.log(restaurant.order?.(0, 1) ?? 'Method does not exsit');
+//console.log(restaurant.orderRisotto?.(0, 1) ?? 'Method does not exsit');
 
 // Arrays
 const users = [
@@ -151,11 +178,13 @@ const users = [
 ]
 
 // const users = []
-console.log(users[0]?.name ?? 'User arry empty');
+// console.log(users[0]?.name ?? 'User arry empty');
 // แบบเก่าจะเขียนแบบนี้
 
-if (users.length > 0) console.log(users[0]?.name);
-else console.log('User arry empty');
+// if (users.length > 0) console.log(users[0]?.name);
+// else console.log('User arry empty');
+
+
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Logical Assignment Operators
